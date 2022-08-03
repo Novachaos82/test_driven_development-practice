d64dbeb76f51091c680403a19b131e2449b6a5b1
Bhavesh Dewangan <ybkdddd@gmail.com>
2022-08-03 05:45:04 +0530

    Reverse String test

---
+++ code/reverseString.js
@@ -0,0 +1,10 @@
+const reverse = (str) => {
+  let newStr = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str[i];
+  }
+  return newStr;
+};
+
+export { reverse };
---
+++ test/reverseString.test.js
@@ -0,0 +1,5 @@
+import { reverse } from "../code/reverseString";
+
+it("check reverse", () => {
+  expect(reverse("yea")).toBe("aey");
+});
